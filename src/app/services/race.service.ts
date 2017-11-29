c9a36abc0855d3c6b3a21f8d9a43876de8a9e3da
Quentin <pc15-salle2@example.com>
2017-11-29 16:04:17 +0100

    add db.json to use [

--- src/app/services/race.service.ts
+++ src/app/services/race.service.ts
@@ -5,45 +5,8 @@ import Race from '../interfaces/race.interface';
 @Injectable()
 export class RaceService {
   
-  races: Race[] = [
-      {
-        id: 1,
-        name: "monte-carlo",
-        ponies:[1,2]
-      },
-      {
-        id: 2,
-        name: "24h du mans",
-        ponies:[1,2,3]
-      },
-      {
-        id: 3,
-        name: "GP de monaco",
-        ponies:[2,3]
-      }
-
-
-  ];
-  ponies: Poney[] = [
-    {
-      id: 1,
-      name: "jackjack",
-      img: "http://ponyracer.ninja-squad.com/assets/images/pony-orange-running.gif",
-      distance: 0
-    },
-    {
-      id: 2,
-      name: "jeanne",
-      img: "http://ponyracer.ninja-squad.com/assets/images/pony-purple-running.gif",
-      distance: 0
-    },
-    {
-      id: 3,
-      name: "serge",
-      img: "http://ponyracer.ninja-squad.com/assets/images/pony-green-running.gif",
-      distance: 0
-    }
-  ];
+  races: Race[] ;
+  ponies: Poney[];
   constructor() { }
 
   getPonies(): Poney[]{
